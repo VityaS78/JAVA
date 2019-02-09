15e40553b03af60794ccfd75a0c9a5143a1e61a2
Vitya <V.Smolyak@seznam.cz>
2019-02-09 16:38:47 +0100

    Переписал через методы

--- src/lesson_3/guess_a_number.java
+++ src/lesson_3/guess_a_number.java
@@ -6,32 +6,46 @@ import java.util.Scanner;
 public class guess_a_number {
 
     public static void main(String[] args) {
-        Random random = new Random();
-        Scanner scanner = new Scanner(System.in);
         int num;
         int answer;
+
+
         do {
-            num = random.nextInt(21);
-            for (int i = 0; i < 3; i++) {
-                System.out.print("Отгадайте число от 0 до 20: ");
-                answer = scanner.nextInt();
+            System.out.print("Введите верхнюю границу дипазона: ");
+            int top = enterANumber();
+            System.out.print("Введите количество попыток: ");
+            int a = enterANumber();
+            num = makeANumber(top + 1);
+            for (int i = 0; i < a; i++) {
+                System.out.print("Отгадайте число от 0 до " + top + " : ");
+                answer = enterANumber();
                 if (num == answer) {
                     System.out.println("Вы угадали!");
                     break;
                 }
-                if (num < answer) {
-                    if (i == 2) {
-                        System.out.println("Вы проиграли!");
-                    } else System.out.println("Загаданное число меньше.");
-                } else {
-                    if (i == 2) {
-                        System.out.println("Вы проиграли!");
-                    } else System.out.println("Загаданное число больше.");
+                if (i == a - 1) {
+                    System.out.println("Вы проиграли!");
+                    break;
                 }
+                if (num < answer) {
+                    System.out.println("Загаданное число меньше.");
+                } else System.out.println("Загаданное число больше.");
+                System.out.println("Осталось попыток " + (a - 1 - i));
             }
             System.out.print("Играем еще раз? 1 если да, иначе нет: ");
-            num = scanner.nextInt();
+            num = enterANumber();
         } while (num == 1);
-        scanner.close();
+    }
+
+    public static int makeANumber(int a) {
+        Random random = new Random();
+        int num = random.nextInt(a);
+        return num;
+    }
+
+    public static int enterANumber() {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        return num;
     }
 }
