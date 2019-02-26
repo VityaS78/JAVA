ac360a5dd5b5948103924ff36df4009b905f27f1
VityaS78 <46622822+VityaS78@users.noreply.github.com>
2019-02-26 18:12:59 +0100

    Delete Dog.java

--- src/lesson_6/Dog.java
+++
@@ -1,9 +0,0 @@
-package lesson_6;
-
-public class Dog extends Animal {
-
-    public Dog(String name, int run, int jamp, int swim) {
-        super(name, run, jamp);
-        this.swim = swim;
-    }
-}
